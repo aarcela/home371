4fe7af714d3fd8a70c4a52a6aceac259befefb0a
workArcela <arcela34@gmail.com>
2022-04-28 11:54:47 -0400

    Adding new minify css and js

---
+++ js/bundle.ts
@@ -0,0 +1,9 @@
+// import "./aos"
+import './counter'
+import './custom'
+import './navbar'
+import './tiny-slider'
+import './glightbox.min.js'
+// import './masonry.pkfd.min.js'
+// import '/js/bootstrap.bundle.min.js'
+// import "./*.js"
